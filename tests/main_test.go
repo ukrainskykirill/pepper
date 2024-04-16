582ad24c8471a676f3d8af799b7ac205eef216ba
ukrainskykirill <ukrainsky.kirill@gmail.com>
2024-04-16 21:04:38 +0300

    pepper:add_switch

--- tests/main_test.go
+++ tests/main_test.go
@@ -6,22 +6,22 @@ import (
 	"os"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/go-testfixtures/testfixtures/v3"
 	_ "github.com/lib/pq"
-	"github.com/gin-gonic/gin"
 	"github.com/ukrainskykirill/pepper/pkg/api"
+	"github.com/ukrainskykirill/pepper/pkg/config"
+	"github.com/ukrainskykirill/pepper/pkg/database"
 	"github.com/ukrainskykirill/pepper/pkg/repositories"
 	"github.com/ukrainskykirill/pepper/pkg/services"
-	"github.com/ukrainskykirill/pepper/pkg/database"
-	"github.com/ukrainskykirill/pepper/pkg/config"
 	"github.com/ukrainskykirill/pepper/pkg/types"
 )
 
 var (
-	db *sql.DB
-	fixtures *testfixtures.Loader
+	db         *sql.DB
+	fixtures   *testfixtures.Loader
 	User1Login string = "johndoe"
-	User1Id string = "4d9db4cd-78d4-4fea-8d66-bfc146f58608"
+	User1Id    string = "4d9db4cd-78d4-4fea-8d66-bfc146f58608"
 	testRouter *gin.Engine
 )
 
@@ -29,12 +29,12 @@ func TestMain(m *testing.M) {
 	var err error
 	db, err = sql.Open("postgres", "user=postgres password=postgres dbname=pepper_test sslmode=disable host=localhost port=32432")
 	if err != nil {
-			fmt.Println("error sql open")
+		fmt.Println("error sql open")
 	}
 	fixtures, err = testfixtures.New(
-			testfixtures.Database(db),
-			testfixtures.Dialect("postgres"),
-			testfixtures.Files("testdata/users.yaml"),
+		testfixtures.Database(db),
+		testfixtures.Dialect("postgres"),
+		testfixtures.Files("testdata/users.yaml"),
 	)
 	if err != nil {
 		fmt.Println("error sql conntext")
