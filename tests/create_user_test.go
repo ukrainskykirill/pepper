b83116f98c799c5a070661a60d66299d19e7d9d2
ukrainskykirill <ukrainsky.kirill@gmail.com>
2024-04-24 20:21:03 +0300

    pepper:add_create_user_table_test

--- tests/create_user_test.go
+++ tests/create_user_test.go
@@ -13,75 +13,49 @@ import (
 	"github.com/ukrainskykirill/pepper/pkg/types"
 )
 
-func TestUserCreatingFailed1(t *testing.T) {
-	t.Run(
-		"Test case: Faild creation user due to invalid phone number (not russian)",
-		func(t *testing.T) {
-			userIn := types.UserInput{
-				Name:  nil,
-				Login: gofakeit.Username(),
-				Phone: "+447800767690",
-			}
-			marshalled, err := json.Marshal(userIn)
-			if err != nil {
-				fmt.Println("impossible to marshall user")
-			}
-			w := httptest.NewRecorder()
-			req, _ := http.NewRequest(
-				"POST",
-				"/user/create",
-				bytes.NewReader(marshalled),
-			)
-			testRouter.ServeHTTP(w, req)
-			assert.Equal(t, 422, w.Code)
-		})
-}
+func TestUserCreatingFailed(t *testing.T) {
+	var userIn types.UserInput
+	err := gofakeit.Struct(&userIn)
+	if err != nil {
+		fmt.Println("error with fakeit struct")
+	}
+    testCases := []struct {
+        testName   string
+        userInput  types.UserInput
+        expectedCode int
+    }{
+        {
+            testName: "Test case: Faild creation user due to invalid phone number (not russian)",
+            userInput: types.UserInput{
+                Name:  nil,
+                Login: gofakeit.Username(),
+                Phone: "+447800767690",
+            },
+            expectedCode: 422,
+        },
+		{
+            testName: "Test case: Successful creation user creation",
+            userInput: userIn,
+            expectedCode: 200,
+        },
+        
+    }
+    for _, tc := range testCases {
+        t.Run(tc.testName, func(t *testing.T) {
+            marshalled, err := json.Marshal(tc.userInput)
+            if err != nil {
+                t.Fatal("impossible to marshall user")
+            }
 
-// func TestUserCreatingFailed2(t *testing.T) {
-// 	t.Run(
-// 		"Test case: Faild creation user due to login already exist",
-// 		func(t *testing.T) {
-// 			prepareTestDatabase()
-// 			userIn := types.UserInput{
-// 				Name:  nil,
-// 				Login: User1Login,
-// 				Phone: "79158865662",
-// 			}
-// 			marshalled, err := json.Marshal(userIn)
-// 			if err != nil {
-// 				fmt.Println("impossible to marshall user")
-// 			}
-// 			w := httptest.NewRecorder()
-// 			req, _ := http.NewRequest(
-// 				"POST",
-// 				"/user/create",
-// 				bytes.NewReader(marshalled),
-// 			)
-// 			testRouter.ServeHTTP(w, req)
-// 			assert.Equal(t, 409, w.Code)
-// 		})
-// }
+            w := httptest.NewRecorder()
+            req, _ := http.NewRequest(
+                "POST",
+                "/user/create",
+                bytes.NewReader(marshalled),
+            )
 
-func TestSuccessfulUserCreating(t *testing.T) {
-	t.Run(
-		"Test case: Successful creation user creation",
-		func(t *testing.T) {
-			var userIn types.UserInput
-			err := gofakeit.Struct(&userIn)
-			if err != nil {
-				fmt.Println("error with fakeit struct")
-			}
-			marshalled, err := json.Marshal(userIn)
-			if err != nil {
-				fmt.Println("impossible to marshall user")
-			}
-			w := httptest.NewRecorder()
-			req, _ := http.NewRequest(
-				"POST",
-				"/user/create",
-				bytes.NewReader(marshalled),
-			)
-			testRouter.ServeHTTP(w, req)
-			assert.Equal(t, 200, w.Code)
-		})
+            testRouter.ServeHTTP(w, req)
+            assert.Equal(t, tc.expectedCode, w.Code)
+        })
+    }
 }
